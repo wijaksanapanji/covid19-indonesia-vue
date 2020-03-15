68fea6ede4a2b11a744bc83e006e49d62cd944f1
wijaksanapanji <wijaksanapanji6@gmail.com>
2020-03-15 10:23:37 +0800

    set meta tag and set publicPath based on env

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  publicPath: "/projects/covid19-indonesia"
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/projects/covid19-indonesia" : "/",
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "Kasus covid19 Indonesia";
+      args[0].description = "Informasi kasus covid19 di Indonesia";
+      return args;
+    });
+  }
 };
